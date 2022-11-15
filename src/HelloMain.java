ece65626ac827cd0f742ae456d03fda280b73d97
MsRioux <jrioux@mvusd.net>
2022-11-15 12:35:08 -0800

    ON T36 pushing simple project.  It was already shared.  CSA cannot push and I need to find out why.  IJ is updated to 2021.3.3.

--- src/HelloMain.java
+++ src/HelloMain.java
@@ -2,5 +2,6 @@ public class HelloMain
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Fun little ditty about Jack and Diane");
     }
 }
